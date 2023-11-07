23130e48f4052edeeda6d4ccfade616792fb6066
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-07 16:51:39 -0600

    Test 1 good fail Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

---
+++ __test__/sudoku.test.js
@@ -0,0 +1,16 @@
+import { Grid } from "./../src/sudoku.js"
+
+describe ("grid", () => {
+    test("should create a grid object with 9 properties", () => {
+        const grid = new Grid(1,2,3,4,5,6,7,8,9);
+        expect(grid.a1).toEqual(1);
+        expect(grid.a2).toEqual(2);
+        expect(grid.a3).toEqual(3);
+        expect(grid.b1).toEqual(4);
+        expect(grid.b2).toEqual(5);
+        expect(grid.b3).toEqual(6);
+        expect(grid.c1).toEqual(7);
+        expect(grid.c2).toEqual(8);
+        expect(grid.c3).toEqual(9);   
+    });
+});
